d94e0cfa5940976d627cd0fffcdcbaff80145120
hayashi <fzlintian_01@163.com>
2020-04-26 23:07:51 +0800

    克隆下来工程修改，提交仓库，并推送远程仓库

--- Git-Peoject/src/com/itcast/git/FirstClass.java
+++ Git-Peoject/src/com/itcast/git/FirstClass.java
@@ -7,11 +7,13 @@ public class FirstClass {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        //注解
 
     }
     public void test1(){
         for (int i = 0; i < 10; i++) {
             System.out.println("你好");
+            System.out.println(i);
         }
     }
 }
